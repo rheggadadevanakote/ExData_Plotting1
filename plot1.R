33b8ea3c8263ed13d85957ca92f7f17fddee5b08
rheggadadevanakote <rheggadadevanakote@outlook.com>
2014-05-10 01:05:44 -0700

    plot1.R comments

--- plot1.R
+++ plot1.R
@@ -2,7 +2,13 @@ plot1<-function(){
   require("sqldf")
   tfile <- "./data/household_power_consumption.txt"
   tsql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+  
+  ##Read data for only Feb 1 and Feb 2 2007.
   data <- read.csv.sql(tfile, tsql, sep=";")
+  
+  ##Get the Global Active Power column and convert it into numeric to facilitate plotting.
   x<-as.numeric(data[,3])
+  
+  ##Plot the x label, title and red color histogram for Global_Active_Power
   hist(x,col="red", main="Global Active Power",xlab="Global Active Power(kilowatts)")
 }
