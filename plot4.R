6c01f803b1f81cf2a506a6e991a61ba723876c79
rheggadadevanakote <rheggadadevanakote@outlook.com>
2014-05-10 04:01:49 -0700

    Plot4.R and plot4.png

---
+++ plot4.R
@@ -0,0 +1,37 @@
+plot4<-function(){
+  require("sqldf")
+  tfile <- "../data/household_power_consumption.txt"
+  tsql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+  
+  ##Read data for only Feb 1 and Feb 2 2007.
+  data <- read.csv.sql(tfile, tsql, sep=";")
+  
+  ##Get the weekdays for the dataset
+  wDays <- strptime(paste(data$Date, data$Time), format='%d/%m/%Y %H:%M:%S')
+  
+  Voltage<-as.numeric(data$Voltage)
+  
+  Global_reactive_power<-as.numeric(data$Global_reactive_power)
+  
+  par(mfrow=c(2,2))
+  
+  ##Plot the 4 graphs
+  
+  with(data,{
+  plot(wDays, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+  
+  plot(wDays,Voltage,type="l",xlab="datetime",ylab="voltage")
+  with(data,plot(wDays,data$Sub_metering_1,type="l",col="black",xlab="",ylab="Energy Sub metering"))
+  lines(wDays,data$Sub_metering_2,col="red")
+  lines(wDays,data$Sub_metering_3,col="blue")
+  legend("topright",lwd=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  plot(wDays,Global_reactive_power,type="l",xlab="datetime",ylab="Global_Reactive_Power")
+  }
+  )
+  
+  ##Copy the plot generated on the screen device to plot1.png
+  dev.copy(png,"./ExData_Plotting1/plot4.png",width=480, height=480)
+  
+  ##close the device.
+  dev.off()
+}
